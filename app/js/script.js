8f5c5e449db468108cdf28f3df43773543daca87
Sheldon H <Sheldon H>
2017-09-26 00:07:05 -0400

    Got search query to work

--- app/js/script.js
+++ app/js/script.js
@@ -2,22 +2,16 @@ var app = {};
 
 app.key = 'MDo0NTQyN2MzYS1hMjA0LTExZTctODg1NS01MzE1NzZlNzljOGI6cjRpVm8wd3k1MktnNkxWQVczSjZhQ1Q2QTVjNFlXSkphdGNZ';
 
-console.log('hello world');
-
-var data = {name: "Bud Light"};
-
-
 app.init = function(){
-	app.ajaxCall(app.key, data);
+	app.ajaxCall(app.key, "beaus");
 }
 
-app.ajaxCall = function(accessKey){
-	var nutritionApi = $.ajax({
-		url: `https://lcboapi.com/products?access_key=${accessKey}`,
-		headers: { 'Authorization': 'Token '+ accesskey }
+app.ajaxCall = function(accessKey, search){
+	$.ajax({
+		url: `https://lcboapi.com/products?q=${search}`,
+		headers: { 'Authorization': 'Token '+ accessKey },
 		type: 'GET',
 		dataType: 'json',
-		data: JSON.stringify(dataObj) ,
 		success: function(data){
 			console.log(data);
 		}
