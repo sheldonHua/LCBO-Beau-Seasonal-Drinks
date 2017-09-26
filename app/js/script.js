0c7f57fde12e0eafcb2bae04eeb3d180e23b414d
Sheldon H <Sheldon H>
2017-09-26 19:24:54 -0400

    filter results - removing non-seasonal drinks

--- app/js/script.js
+++ app/js/script.js
@@ -2,11 +2,11 @@ var app = {};
 
 app.key = 'MDo0NTQyN2MzYS1hMjA0LTExZTctODg1NS01MzE1NzZlNzljOGI6cjRpVm8wd3k1MktnNkxWQVczSjZhQ1Q2QTVjNFlXSkphdGNZ';
 
-app.init = function(){
+app.init = function() {
 	app.ajaxCall(app.key, "beaus");
 }
 
-app.ajaxCall = function(accessKey, search){
+app.ajaxCall = function(accessKey, search) {
 	$.ajax({
 		url: `https://lcboapi.com/products?q=${search}`,
 		headers: { 'Authorization': 'Token '+ accessKey },
@@ -14,11 +14,24 @@ app.ajaxCall = function(accessKey, search){
 		dataType: 'json',
 		success: function(data){
 			console.log(data);
+			app.filter(data.result);
 		}
 	});	
 }
 
+app.filter = function(data) {
 
-$(function(){
+	// return seasonal drinks
+	var seasonal = data.filter(function(drink) {
+		return drink.is_seasonal === true;
+	});
+
+	
+
+
+	
+}
+
+$(function() {
 	app.init();
 });
