d4c7673595124d5428e80ded1069688e2eb2e89f
Sheldon H <Sheldon H>
2017-09-26 21:52:00 -0400

    use product ids to get store locations

--- app/js/script.js
+++ app/js/script.js
@@ -3,34 +3,52 @@ var app = {};
 app.key = 'MDo0NTQyN2MzYS1hMjA0LTExZTctODg1NS01MzE1NzZlNzljOGI6cjRpVm8wd3k1MktnNkxWQVczSjZhQ1Q2QTVjNFlXSkphdGNZ';
 
 app.init = function() {
-	app.ajaxCall(app.key, "beaus");
+	app.getDrink(app.key, "beaus");
+	//app.getStores(app.key);
 }
 
-app.ajaxCall = function(accessKey, search) {
+app.getDrink = function(accessKey, search) {
 	$.ajax({
 		url: `https://lcboapi.com/products?q=${search}`,
 		headers: { 'Authorization': 'Token '+ accessKey },
 		type: 'GET',
 		dataType: 'json',
 		success: function(data){
-			console.log(data);
-			app.filter(data.result);
+			app.getProductId(data.result);
 		}
 	});	
 }
 
-app.filter = function(data) {
+app.getStores = function(accessKey, product) {
+	for (i = 0; i < product.length; i++){
+		$.ajax({
+			url: `https://lcboapi.com/stores?product_id=${product[i]}`,
+			headers: { 'Authorization': 'Token '+ accessKey },
+			type: 'GET',
+			dataType: 'json',
+			success: function(data){
+				console.log(data);
+			}
+		});	
+	}
+	//once loop is done display html
+
+}
+
+app.getProductId = function(data) {
 	// return seasonal drinks
-	app.seasonal = data.filter(function(drink) {
+	var seasonal = data.filter(function(drink) {
 		return drink.is_seasonal === true;
 	});
+
 	// return array of product id(s)
-	app.productId = app.seasonal.map(function(drink) {
+	var productId = seasonal.map(function(drink) {
 		return drink.id;
 	});
 
-	console.log(app.seasonal);
-	console.log(app.productId);
+	//console.log(productId);
+
+	app.getStores(app.key, productId);
 }
 
 $(function() {
